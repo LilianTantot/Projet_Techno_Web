33c283aa4ef11d474734c0ce75a2b02273fe5880
Lilian <Lilian@LAPTOP-Q3ARS6QF>
2021-02-27 10:16:41 +0100

    application properties

--- src/main/java/com/example/demo/ProjetTechnoWebApplication.java
+++ src/main/java/com/example/demo/ProjetTechnoWebApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjetTechnoWebApplication {
-
+	//test 1
+	//test2
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetTechnoWebApplication.class, args);
 	}
