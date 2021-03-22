19c16ec0116e044341f4289d7c6e05a8cfe96351
unknown <79005488+AchilleM99@users.noreply.github.com>
2021-03-22 15:05:26 +0100

    Test

--- src/main/java/com/example/demo/Achat.java
+++ src/main/java/com/example/demo/Achat.java
@@ -9,7 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
-
+//test
 @Entity
 public class Achat {
 	@Id
