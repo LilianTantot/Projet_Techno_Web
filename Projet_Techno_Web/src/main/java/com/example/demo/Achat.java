063b550104c169f3609343aaa09638ab3531cd4c
LilianTantot <tantotlilian@gmail.com>
2021-03-22 14:49:33 +0100

    Test

---
+++ Projet_Techno_Web/src/main/java/com/example/demo/Achat.java
@@ -0,0 +1,67 @@
+package com.example.demo;
+
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Achat {
+	@Id
+	@GeneratedValue( strategy = GenerationType.AUTO)
+	private Long IDachat;
+	private int prixAchat;
+	private Long date;
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private Client client;
+	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private List<Article> article;
+	
+	public Achat() {super();}
+
+	public Long getIDachat() {
+		return IDachat;
+	}
+
+	public void setIDachat(Long iDachat) {
+		IDachat = iDachat;
+	}
+
+	public int getPrixAchat() {
+		return prixAchat;
+	}
+
+	public void setPrixAchat(int prixAchat) {
+		this.prixAchat = prixAchat;
+	}
+
+	public Long getDate() {
+		return date;
+	}
+
+	public void setDate(Long date) {
+		this.date = date;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public List<Article> getArticle() {
+		return article;
+	}
+
+	public void setArticle(List<Article> article) {
+		this.article = article;
+	}
+	
+}
