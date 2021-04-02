2327beb16803b1118ce3fc492e800513d373cf80
LilianTantot <tantotlilian@gmail.com>
2021-04-02 13:27:41 +0200

    affichage vitrine

--- src/main/resources/public/js/Index.js
+++ src/main/resources/public/js/Index.js
@@ -7,5 +7,11 @@ $(document).ready(function(){
 	$('#NomUsername').click(function(){
 			window.sessionStorage.clear();
 	});
+	
+	$.get("http://localhost:8080/API/article",function(resp){
+  		$.each(resp,function(index,item){
+    		$('#Image1').append('<img src="'+item.image+'">');
+		})
+	})
 });
 	
--- src/main/resources/public/js/Shop.js
+++ src/main/resources/public/js/Shop.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 	//affichage des article présent dans la table Article dans la page shop 
 	$.get('http://localhost:8080/API/article',function(resp){
 		$.each(resp, function(index, item){
-			$('#list_article').append('<li id="article_'+item.idart+'">'+item.nom+' taille : '+item.taille+' couleur : '+item.couleur + ' sexe : '+ item.sexe+ ' ');
+			$('#list_article').append('<li id="article_'+item.idart+'">'+item.nom+'<br>Taille : '+item.taille+'<br>Couleur : '+item.couleur + '<br>Sexe : '+ item.sexe+ '<br> <img src="'+item.image+'"><br>Description : '+item.description+'<br><br>');
 		})
 	})
 });
